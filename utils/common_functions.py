1bde923199d6a741307dc5c8c9a96795bc4b465f
mavendataguy <mavendataguy>
2022-10-03 14:03:00 +0000

    commons_temp

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -21,4 +21,32 @@ sys.path.append("/Workspace/Repos/rana.aurangzeb@hotmail.com/cicd-pipeline")
 
 # COMMAND ----------
 
+from pyspark.sql.types import StructField, StructType,StringType,DateType
+from datetime import datetime,date
+log_schema=StructType([
+    StructField('notebookName',StringType(), False),
+    StructField('functionName',StringType(), False),
+    StructField('source',StringType(), True),
+    StructField('target',StringType(), True),
+    StructField('eventDate',DateType(), False),
+    StructField('eventTime',StringType(), False), #datetime.now().strftime("%H:%M:%S %p")
+    StructField('remarks',StringType(), True)  
+                               ])
+#display(spark.sql(f"create database misc OPTIMIZE {misc.test}"))
+
+# COMMAND ----------
+
+def append_log_data(logSchema,notebookName,functionName,source,target,eventDate,eventTime,remarks,table_name, partition_column):
+    #output_df = re_arrange_partition_column(input_df, partition_column)
+    initialMessage=[{"notebookName":notebookName,"functionName":functionName,"source":source,"target":target,"eventDate":eventDate,"eventTime":eventTime,"remarks":remarks}]
+    df=spark.createDataFrame(data=initialMessage,schema=logSchema)
+    #output_df.write.mode("overwrite").partitionBy(partition_column).format("parquet").saveAsTable(f"{db_name}.{table_name}")
+    spark.conf.set("spark.sql.sources.partitionOverwriteMode","dynamic")
+    if (spark._jsparkSession.catalog().tableExists(f"{logSchema}.{table_name}")):
+        df.write.mode("append").insertInto(f"{logSchema}.{table_name}")
+    else:
+        df.write.mode("overwrite").partitionBy(partition_column).format("delta").saveAsTable(f"{logSchema}.{table_name}")
+
+# COMMAND ----------
+
 
