bed27aab4a081eb7c63f504b323272033172ed87
mavendataguy <mavendataguy>
2022-10-03 14:24:16 +0000

    minor

--- src/prepare_etl_ecom1.ipynb.py
+++ src/prepare_etl_ecom1.ipynb.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 def initialse(db_name):
-    spark.sql(f"create database if not exists misc location 'dbfs:/mnt/mavendataguy/silver/db/{ecom}'")    
+    spark.sql(f"create database if not exists misc location 'dbfs:/mnt/mavendataguy/silver/db/{db_name}'")    
 
 # COMMAND ----------
 
