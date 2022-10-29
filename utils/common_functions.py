2b64ce454777dad67fc3a20eb35b5325dd9c66ef
mavendataguy <mavendataguy>
2022-10-29 09:44:49 +0000

    streamlined

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,5 +1,7 @@
 # Databricks notebook source
 import json
+import sys
+from pyspark.sql.functions import current_timestamp
 input_params={}
 spark.conf.set("spark.databricks.delta.schema.autoMerge.enabled",True)
 raw_folder_path="/mnt/mavendataguy/input/ecom_growth"
@@ -9,19 +11,11 @@ processed_folder_path="/mnt/mavendataguy/silver/ecom_growth"
 v_file_date="2021-12-15"
 input_params['v_file_date']=v_file_date
 input_params['raw_folder_path']=raw_folder_path
+input_params['database']="ecom"
 #files_path= {"products":f"{raw_folder_path}/{v_file_date}/products", "sessions.csv":f"{raw_folder_path}/{v_file_date}/sessions.csv"}
-
-# Writing configs to JSON and can be read back as well
-#df = spark.read.json(sc.parallelize([input_params]))
-#df.coalesce(1).write.mode("overwrite").format('json').save('/mnt/mavendataguy/input/params.json')
 
 # COMMAND ----------
 
-import sys
-import json
-#sys.path.append("/Workspace/Repos/rana.aurangzeb@hotmail.com/cicd-pipeline")
-#print(sys.path)
-#datetime.now().strftime("%H:%M:%S %p")
 config_file='/dbfs/mnt/mavendataguy/bronze/config/param.json'
 with open(config_file,'w') as outFile:
     config_data=json.dump(input_params,outFile)
@@ -33,24 +27,17 @@ with open(config_file,'w') as outFile:
 
 # COMMAND ----------
 
-def create_temp_view (input_map, frmt):
-    for key in input_map:
-        spark.read.format(frmt).option("header",True).load(input_map.get(key)).createOrReplaceTempview(key)
-        
-#create_temp_view (files_path, "csv")
-
-# COMMAND ----------
-
-from pyspark.sql.functions import current_timestamp
 def add_ingestion_date(input_df):
     output_df = input_df.withColumn("ingestion_date", current_timestamp())
     return output_df
 
 # COMMAND ----------
 
-def read_file(path,date,partition_column):
-    df= spark.read.format("parquet").load(f"{path}")#.filter(partition_column='2020-01-01')
-    #return {"return_df":df}
+def read_file(path,custom_schema,headers="False",file_format="parquet",partition_col="date"):
+    df= spark.read.format(file_format)\
+    .schema(custom_schema)\
+    .option("header", headers)\
+    .load(f"{path}")#.filter(date_col="{date}")
     return df
 
 # COMMAND ----------
@@ -86,12 +73,12 @@ def overwrite_partition(input_df, db_name, table_name, partition_column):
 
 # COMMAND ----------
 
-def merge_delta_data(input_df, db_name, table_name, folder_path, merge_condition, partition_column):
+def merge_delta_data(input_df, db_name, table_name, db_path,merge_condition, partition_column):
     spark.conf.set("spark.databricks.optimizer.dynamicPartitionPruning","true")
 
     from delta.tables import DeltaTable
     if (spark._jsparkSession.catalog().tableExists(f"{db_name}.{table_name}")):
-        deltaTable = DeltaTable.forPath(spark, f"{folder_path}/{table_name}")
+        deltaTable = DeltaTable.forPath(spark, f"{db_path}/{table_name}")
         deltaTable.alias("tgt").merge(
         input_df.alias("src"),
         merge_condition) \
@@ -102,7 +89,6 @@ def merge_delta_data(input_df, db_name, table_name, folder_path, merge_condition
         input_df.write.mode("overwrite").partitionBy(partition_column).format("delta").saveAsTable(f"{db_name}.{table_name}")
 
 # COMMAND ----------
-
 
 from pyspark.sql.types import StructField, StructType,StringType,DateType
 from datetime import datetime,date
@@ -118,8 +104,8 @@ log_schema=StructType([
 
 # COMMAND ----------
 
-def append_log_data(logSchema,notebookName,functionName,source,target,eventDate,eventTime,remarks,table_name, partition_column):
-    initialMessage=[{"notebookName":notebookName,"functionName":functionName,"source":source,"target":target,"eventDate":eventDate,"eventTime":eventTime,"remarks":remarks}]
+def append_log_data(logSchema,notebookName,functionName,source,target,eventDate,remarks,table_name, partition_column):
+    initialMessage=[{"notebookName":notebookName,"functionName":functionName,"source":source,"target":target,"eventDate":eventDate,"eventTime":datetime.now().strftime("%H:%M:%S %p"),"remarks":remarks}]
     df=spark.createDataFrame(data=initialMessage,schema=logSchema)
     spark.conf.set("spark.sql.sources.partitionOverwriteMode","dynamic")
     if (spark._jsparkSession.catalog().tableExists(f"{logSchema}.{table_name}")):
@@ -128,6 +114,15 @@ def append_log_data(logSchema,notebookName,functionName,source,target,eventDate,
         df.write.mode("overwrite").partitionBy(partition_column).format("delta").saveAsTable(f"{logSchema}.{table_name}")
         #display(spark.sql(f"create database misc OPTIMIZE {misc.test}"))
 
+# COMMAND ----------
+
+def create_temp_view (input_map, frmt):
+    for key in input_map:
+        spark.read.format(frmt).option("header",True).load(input_map.get(key)).createOrReplaceTempview(key)
+
+# COMMAND ----------
+
+spark.catalog.listTables()
 
 # COMMAND ----------
 
